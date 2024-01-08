95c004ae94f70c8e00aef3dc3b16ef16e24ac856
Rahul Patwal <patwalrahul3@gmail.com>
2024-01-08 12:17:07 +0530

    FEAT: crud for airport

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,9 +6,11 @@ const {InfoController} = require('../../controllers')
 const airplaneRouter = require('./airplane-routes')
 
 const cityRouter = require('./city-routes');
+const airportRouter = require('./airport-routes');
 
 router.use('/airplane', airplaneRouter);
 router.use('/cities', cityRouter);
+router.use('/airport', airportRouter);
 
 router.get('/info', InfoController.info);
 
