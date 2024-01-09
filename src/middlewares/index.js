1b4e0a816f30fb4fc7992d36a08b4a9b608e277e
Rahul Patwal <patwalrahul3@gmail.com>
2024-01-09 11:21:47 +0530

    FEAT: added flight create features

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     AirplaneMiddlewares : require('./airplane-middleware'),
-    AirportMiddlewares: require('./airport-middleware')
+    AirportMiddlewares: require('./airport-middleware'),
+    FlightMiddlewares : require('./flight-middleware')
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,10 +7,13 @@ const airplaneRouter = require('./airplane-routes')
 
 const cityRouter = require('./city-routes');
 const airportRouter = require('./airport-routes');
+const flightRouter = require('./flight-routes')
+
 
 router.use('/airplane', airplaneRouter);
 router.use('/cities', cityRouter);
 router.use('/airport', airportRouter);
+router.use('/flight', flightRouter )
 
 router.get('/info', InfoController.info);
 
